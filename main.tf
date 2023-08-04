5bf798cd2f3fcd193423a4b214d50d174a21e9f3
gaddamrk <raj>
2023-08-04 16:40:54 +0530

    jr

--- main.tf
+++ main.tf
@@ -7,8 +7,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.cidr_block
+  count       = length(var.subnets_cidr)
+  vpc_id      = aws_vpc.main.id
+  cidr_block  = var.subnets_cidr[count.index]
+
 
   tags       = merge(
     local.common_tags,
