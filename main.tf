58627722dad0522cee280d04935f2fe57b2d4a39
gaddamrk <raj>
2023-08-11 16:09:43 +0530

    jr

--- main.tf
+++ main.tf
@@ -31,9 +31,9 @@ resource "aws_internet_gateway" "igw" {
   )
 }
 
-resource "aws_eip" "ngw-eip" {
-  vpc =  true
-}
+#resource "aws_eip" "ngw-eip" {
+#  vpc =  true
+#}
 #
 #resource "aws_nat_gateway" "ngw" {
 #  allocation_id = aws_eip.ngw-eip.id
