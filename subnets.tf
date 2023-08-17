8156e96058f72819a9dd462f01538ccbedb01f9c
gaddamrk <raj>
2023-08-17 16:40:00 +0530

    jr

--- subnets.tf
+++ subnets.tf
@@ -8,12 +8,13 @@ module "public_subnets" {
     cidr_block = each.value.cidr_block
     name = each.value.name
     internet_gw = lookup(each.value, "internet_gw", false)
-  #  nat_gw = lookup(each.value, "nat_gw", false)
+    nat_gw = lookup(each.value, "nat_gw", false)
 
     vpc_id = aws_vpc.main.id  //these are just local which are created
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
     tags = local.common_tags
     gateway_id = aws_internet_gateway.igw.id
+
 }
 
 module "private_subnets" {
@@ -31,6 +32,5 @@ module "private_subnets" {
   vpc_id = aws_vpc.main.id  //these are just local which are created
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   tags = local.common_tags
-  gateway_id = aws_internet_gateway.igw.id
   nat_gw_id = aws_nat_gateway.ngw.id
 }
