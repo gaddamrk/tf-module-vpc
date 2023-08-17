0b61729a3a8b64310015516b4854ca330bd46cf2
gaddamrk <raj>
2023-08-17 13:19:08 +0530

    jr

--- subnets.tf
+++ subnets.tf
@@ -1,22 +1,31 @@
 #module "subnets" {
-#  source = "./subnets"
+#  source =
 #
-#  availability_zone = var.availability_zone
-#  default_vpc_id = var.default_vpc_id
-#  env = var.env
-#
-#  for_each = var.subnets
-#  cidr_block = each.value.cidr_block
-#  name = each.value.name
-#  internet_gw = lookup(each.value, "internet_gw", false)
-##  nat_gw = lookup(each.value, "nat_gw", false)
-#
-#  vpc_id = aws_vpc.main.id  //these are just local which are created
-#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-#  tags = local.common_tags
-#  gateway_id = aws_internet_gateway.igw.id
-#
-#
-#}
 
+#
 //i am just trying to segrigate the things
+
+module "public_subnets" {
+  source =  "./subnets"
+    availability_zone = var.availability_zone
+    default_vpc_id = var.default_vpc_id
+    env = var.env
+
+    for_each = var.public_subnets
+    cidr_block = each.value.cidr_block
+    name = each.value.name
+    internet_gw = lookup(each.value, "internet_gw", false)
+  #  nat_gw = lookup(each.value, "nat_gw", false)
+
+    vpc_id = aws_vpc.main.id  //these are just local which are created
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    tags = local.common_tags
+    gateway_id = aws_internet_gateway.igw.id
+
+
+
+
+
+
+
+}
